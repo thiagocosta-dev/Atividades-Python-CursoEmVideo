c024d0f3279dd40113a97cd31aa6ed360e806e9c
Thiago Costa <thiago.devpython@gmail.com>
2021-03-30 19:52:19 -0300

    Enviando ex003

---
+++ ex003.py
@@ -0,0 +1,13 @@
+'''
+DESAFIO 003
+
+Crie um programa que leia dois números e mostre a soma entre eles.
+
+'''
+
+n1 = int(input('Digite um número:\n'))
+n2 = int(input('Digite outro:\n'))
+
+resultado = n1 + n2
+
+print(f'A soma entre {n1} + {n2} = {resultado}')
