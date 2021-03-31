779d3e35f1d39a85ae6060fb51b4c0bd87d6ebe9
Thiago Costa Pereira <59802768+thiagocosta-dev@users.noreply.github.com>
2021-03-31 10:37:33 -0300

    Add ex005

---
+++ ex005.py
@@ -0,0 +1,12 @@
+'''
+DESAFIO 005 
+
+Crie um programa que peça ao usuário para digita um número inteiro, e em seguida mostre seu antecessor e seu sucessor.
+
+'''
+
+num = int(input('Digite um número: '))
+ant = num - 1
+suc = num + 1
+print(f'O antecessor de {num} é {ant}\nE o sucessor é {suc}.')
+
