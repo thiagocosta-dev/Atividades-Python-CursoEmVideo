a0491f3c8a3c20ebdc5eaef665d8bddd533df9bd
Thiago Costa <thiago.devpython@gmail.com>
2021-04-11 02:47:03 -0300

    Add ex

---
+++ ex048.py
@@ -0,0 +1,15 @@
+'''
+DESAFIO 048
+
+Faça um programa que calcule a soma entre todos números impares que são multiplos de três e que
+se encontram no intervalo de 1 até 500.
+'''
+soma = 0
+contador = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        contador = contador + 1
+        soma = soma + c
+        
+print(f'A soma de todos os {contador} valores solicitados é: {soma}')
+
